95e6e102ff53337d09ae97c90049c3ecb2e788c9
ryanRoberge <roberger@wit.edu>
2016-09-08 20:10:42 -0400

    up2date

--- app/src/main/java/com/example/sokoll/ass/expert_or_reg.java
+++ app/src/main/java/com/example/sokoll/ass/expert_or_reg.java
@@ -10,6 +10,6 @@ public class expert_or_reg extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expert_or_reg);
 
-//
+///;.;.
     }
 }
